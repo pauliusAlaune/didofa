c07c025df5c1ad68a63455a8da60e8957f6effac
pauliusAlaune <paulius.alaune@gmail.com>
2018-07-13 23:37:36 +0300

    work with header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+$(document).ready(function() { 
+    $("#menuToggle").click(function(){
+        $("#nav-icon").toggleClass("open");
+        if($("#nav-icon").hasClass("open")) {
+//            $(".navWrapper").delay(300).css({"transform": "translateX(0%)"});
+            $(".navWrapper").delay(300).addClass('active');
+        } else {
+//            $(".navWrapper").delay(1000).css({"transform": "translateX(-100%)"});
+            $(".navWrapper").delay(300).removeClass('active');
+        }
+    });
+});
+
