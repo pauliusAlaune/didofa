e1223dc0df9415a65ec7ab813f1640543a5c53eb
pauliusAlaune <paulius.alaune@gmail.com>
2018-07-14 23:54:58 +0300

    complete token section for desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,21 @@ $(document).ready(function() {
     $("#menuToggle").click(function(){
         $("#nav-icon").toggleClass("open");
         if($("#nav-icon").hasClass("open")) {
-//            $(".navWrapper").delay(300).css({"transform": "translateX(0%)"});
             $(".navWrapper").delay(300).addClass('active');
         } else {
-//            $(".navWrapper").delay(1000).css({"transform": "translateX(-100%)"});
             $(".navWrapper").delay(300).removeClass('active');
         }
     });
+    
+    addPercentages();
+    
+    function addPercentages() {
+        let percentagesNums =  document.querySelectorAll('.percentagesNum');
+        let percentagesLine =  document.querySelectorAll('.percentagesLine');        
+        for(var i=0; i< percentagesNums.length;i++){
+            let thisText = percentagesNums[i].textContent;
+            percentagesLine[i].style.width = thisText;
+        }
+    }
 });
 
